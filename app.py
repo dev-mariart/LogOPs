d6d0c1a64b1542cbba6aa17744304158b77888d5
eugenniart-16 <eugenia.toranza@gmail.com>
2023-11-23 18:02:52 -0300

    first commit

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from datetime import date
 from functools import wraps
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key" 
+app.secret_key = "tu_clave_secreta"  # Cambia esto por una clave secreta segura
 
-
+# Configuración de la conexión a la base de datos
 conn = psycopg2.connect(
     database="logOps_db",
     user="postgres",
@@ -15,7 +15,7 @@ conn = psycopg2.connect(
     port="5432"
 )
 
-
+# Decorador para verificar la autenticación
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -30,74 +30,74 @@ def login():
 
     if request.method == "POST":
         try:
-            employee_number = int(request.form["employee_number"])
+            numero_empleado = int(request.form["employee_number"])
             password = request.form["password"]
 
             cur = conn.cursor()
-            cur.execute("SELECT employee_number, name, last_name FROM employees WHERE employee_number = %s", (employee_number,))
-            user = cur.fetchone()
+            cur.execute("SELECT numero_empleado, nombre, apellido FROM empleados WHERE numero_empleado = %s", (numero_empleado,))
+            usuario = cur.fetchone()
             cur.close()
 
-            if user and str(user[0]) == password:
+            if usuario and str(usuario[0]) == password:
                 session['logged_in'] = True
-                session['user_id'] = employee_number
-                return redirect(url_for("dashboard", employee_number=employee_number))
+                session['user_id'] = numero_empleado
+                return redirect(url_for("dashboard", numero_empleado=numero_empleado))
             else:
-                error = "Incorrect credentials. Please try again."
+                error = "Los datos son incorrectos. Por favor, inténtelo de nuevo."
         except ValueError:
-            error = "Employee number should be a number."
+            error = "El número de empleado debe ser un número."
 
     return render_template("login.html", error=error)
 
-@app.route("/dashboard/<int:employee_number>")
+@app.route("/dashboard/<int:numero_empleado>")
 @login_required
-def dashboard(employee_number):
+def dashboard(numero_empleado):
     cur = conn.cursor()
-    cur.execute("SELECT name, last_name FROM employees WHERE employee_number = %s", (employee_number,))
-    employee_info = cur.fetchone()
+    cur.execute("SELECT nombre, apellido FROM empleados WHERE numero_empleado = %s", (numero_empleado,))
+    empleado_info = cur.fetchone()
     cur.close()
 
-    if employee_info:
-        name, last_name = employee_info
-        return render_template("dashboard.html", name=name, last_name=last_name, employee_number=employee_number)
+    if empleado_info:
+        nombre, apellido = empleado_info
+        return render_template("dashboard.html", nombre=nombre, apellido=apellido, numero_empleado=numero_empleado)
     else:
-        return "Error: Employee information not found."
+        return "Error: Información del empleado no encontrada."
 
 @app.route("/logout", methods=["GET"])
 def logout():
     session.clear()
     return redirect(url_for("login"))
 
-@app.route("/mark/<int:employee_number>/<mark_type>", methods=["POST"])
+@app.route("/marcar/<int:numero_empleado>/<tipo_marca>", methods=["POST"])
 @login_required
-def mark(employee_number, mark_type):
+def marcar(numero_empleado, tipo_marca):
     try:
-        
+        # Verifica si ya existe una marca para el tipo y el empleado en el día actual
         cur = conn.cursor()
-        cur.execute("SELECT COUNT(*) FROM time_marks WHERE employee_number = %s AND mark_type = %s AND timestamp::date = %s",
-                    (employee_number, mark_type, date.today()))
-        existing_mark = cur.fetchone()[0]
+        cur.execute("SELECT COUNT(*) FROM marcas_horario WHERE numero_empleado = %s AND tipo_marca = %s AND timestamp::date = %s",
+                    (numero_empleado, tipo_marca, date.today()))
+        marca_existente = cur.fetchone()[0]
         cur.close()
 
-        if existing_mark > 0:
-            error_message = "You have already marked today."
-            return jsonify({"error": error_message}), 400
+        if marca_existente > 0:
+            mensaje_error = "Ya has marcado hoy."
+            return jsonify({"error": mensaje_error}), 400
 
         data = request.get_json()
-        mark_datetime = data.get("mark_datetime")
-        mark_date = mark_datetime.split()[0]
-        mark_time = mark_datetime.split()[1]
+        fecha_hora_marca = data.get("fecha_hora_marca")
+        fecha_marca = fecha_hora_marca.split()[0]
+        hora_marca = fecha_hora_marca.split()[1]
 
         cur = conn.cursor()
-        cur.execute("INSERT INTO time_marks (employee_number, mark_type, timestamp, mark_time) VALUES (%s, %s, %s, %s)",
-                    (employee_number, mark_type, mark_date, mark_time))
+        cur.execute("INSERT INTO marcas_horario (numero_empleado, tipo_marca, timestamp, hora_marca) VALUES (%s, %s, %s, %s)",
+                    (numero_empleado, tipo_marca, fecha_marca, hora_marca))
         conn.commit()
         cur.close()
 
-        success_message = "Mark registered successfully."
-        return jsonify({"message": success_message}), 200
+        mensaje_exito = "Marca registrada correctamente."
+        return jsonify({"message": mensaje_exito}), 200
     except Exception as e:
-        return jsonify({"error": f"Error while marking: {str(e)}"}), 500
+        return jsonify({"error": f"Error al marcar: {str(e)}"}), 500
     
 
 if __name__ == "__main__":
